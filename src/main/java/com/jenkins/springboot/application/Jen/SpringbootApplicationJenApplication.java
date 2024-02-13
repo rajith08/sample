9594021475f265812834832717eeef9f39aa65ed
rajith08 <rajithbhargav036@gmail.com>
2024-02-13 17:43:18 +0530

    first commit

---
+++ src/main/java/com/jenkins/springboot/application/Jen/SpringbootApplicationJenApplication.java
@@ -0,0 +1,13 @@
+package com.jenkins.springboot.application.Jen;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringbootApplicationJenApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringbootApplicationJenApplication.class, args);
+	}
+
+}
